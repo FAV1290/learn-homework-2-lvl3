2ac93c5306aa663014f6379811b3f89664478e5b
Alexey <fav1290@gmail.com>
2023-06-05 06:12:14 +0300

    slightly changed structure

--- test.py
+++ test.py
@@ -2,16 +2,20 @@
 import random
 
 
-def create_cities_base(filename):
+def read_cities_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file_handler:
+        cities_list = file_handler.readlines()
+    return cities_list
+
+
+def create_cities_base(cities_list):
     cities_base = {}
-    with open(filename, 'r', encoding='utf-8') as file_handler:
-        cities = file_handler.readlines()
-    for city in cities:
-        city_first_letter = city.lower().strip()[0]
-        if cities_base.get(city_first_letter) is None:
-            cities_base[city_first_letter] = [city.lower().strip(),]
+    for city in cities_list:
+        city = city.lower().strip()
+        if cities_base.get(city[0]) is None:
+            cities_base[city[0]] = [city]
         else:
-            cities_base[city_first_letter].append(city.lower().strip())
+            cities_base[city[0]].append(city)
     return cities_base
 
 
@@ -29,47 +33,49 @@ def check_input(user_input, cities_base, used_cities, necessary_letter):
 
 
 def letter_for_next_player(word, banned_letters):
-    if word[-1] in banned_letters and word[-2] in banned_letters:
-        return word[-3]
-    elif word[-1] in banned_letters:
-        return word[-2]
-    else:
-        return word[-1]
+    for letter in reversed(word):
+        if letter not in banned_letters:
+            return letter
 
 
 def generate_bot_answer(cities_base, answer_letter, used_cities):
     answer = None
     while answer is None or answer in used_cities:
-        random_city_number = random.randint(0, len(cities_base[answer_letter]) - 1)
-        answer = cities_base[answer_letter][random_city_number]
+        answer = ''.join(random.choices(cities_base[answer_letter]))
     return answer
 
 
+def generate_not_ok_reaction(check_result, necessary_letter):
+    if check_result == 'exit':
+        return 'Бот: Очень жаль! До свидания'
+    elif check_result == 'unknown city':
+        return'Бот: Не знаю такого города! Попробуйте еще раз'
+    elif check_result == 'wrong letter':
+        return f'Бот: Город должен начинаться с буквы {necessary_letter.upper()}. Попробуйте еще раз'
+    elif check_result == 'used city':
+        return 'Бот: Этот город уже был! Попробуйте еще раз'
+
+
 def main():
-    cities_base = create_cities_base('cities.txt')
-    banned_letters = ('ё', 'ь', 'ы', 'ъ', 'й')
+    cities_list = read_cities_file('cities.txt') 
+    cities_base = create_cities_base(cities_list)
+    banned_letters = set(['ё', 'ь', 'ы', 'ъ', 'й'])
     used_cities = []
     necessary_letter = None
     user_input = None
-    print('Игра началась! Введите название города или Выход для завершения игры')
+    print("Игра началась! Введите название города или 'выход' для завершения игры")
     while user_input is None or user_input != 'выход':
         user_input = input('Игрок: ').lower().strip()
         check_result = check_input(user_input, cities_base, used_cities, necessary_letter)
-        if check_result == 'exit':
-            print('Бот: Очень жаль! До свидания')
-        elif check_result == 'unknown city':
-            print('Бот: Не знаю такого города! Попробуйте еще раз')
-        elif check_result == 'wrong letter':
-            print(f'Бот: Город должен начинаться с буквы {necessary_letter.upper()}. Попробуйте еще раз')
-        elif check_result == 'used city':
-            print('Бот: Этот город уже был! Попробуйте еще раз')
-        else:
+        if check_result == 'ok':
             used_cities.append(user_input)
             answer_letter = letter_for_next_player(user_input, banned_letters)
             answer = generate_bot_answer(cities_base, answer_letter, used_cities)
             used_cities.append(answer)
             necessary_letter = letter_for_next_player(answer, banned_letters) 
             print(f'Бот: {answer.capitalize()} (Ваш город должен начинаться с буквы {necessary_letter.upper()})')
+        else:
+            print(generate_not_ok_reaction(check_result, necessary_letter))
 
 
 # Далее:
